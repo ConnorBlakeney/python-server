dd7dc64c42d12f5adec61a85c22159643311d41f
Connor Blakeney <connorblakeney@yahoo.com>
2020-10-06 15:40:51 -0500

    first commit

---
+++ animals/request.py
@@ -0,0 +1,35 @@
+ANIMALS = [
+    {
+      "id": 1,
+      "name": "Doodles",
+      "breed": "Poodle",
+      "customerId": 1,
+      "locationId": 1
+    },
+    {
+      "id": 2,
+      "name": "Sprinkles",
+      "breed": "Shih Tzu",
+      "customerId": 2,
+      "locationId": 2
+    },
+    {
+      "id": 3,
+      "name": "Fuzzy",
+      "breed": "Sheepdog",
+      "customerId": 3,
+      "locationId": 2
+    },
+    {
+      "name": "Harry",
+      "breed": "Great Dane",
+      "locationId": 1,
+      "treatment": "",
+      "customerId": 2,
+      "id": 4
+    }
+]
+
+
+def get_all_animals():
+    return ANIMALS
---
+++ employees/request.py
@@ -0,0 +1,36 @@
+EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Big Hal",
+      "locationId": 2,
+      "animalId": 1
+    },
+    {
+      "id": 2,
+      "name": "Big Abe",
+      "locationId": 2,
+      "animalId": 2
+    },
+    {
+      "id": 3,
+      "name": "Big Jude",
+      "locationId": 1,
+      "animalId": 1
+    },
+    {
+      "id": 4,
+      "name": "Big Germ",
+      "locationId": 1,
+      "animalId": 2
+    },
+    {
+      "name": "Jake",
+      "locationId": 1,
+      "animalId": 3,
+      "id": 5
+    }
+]
+
+
+def get_all_employees():
+    return EMPLOYEES
