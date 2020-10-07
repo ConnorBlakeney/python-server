a95dff7896a6f549bce96b746f41b52be44b0081
Connor Blakeney <connorblakeney@yahoo.com>
2020-10-07 16:05:36 -0500

    chapter 4 done

--- customers/request.py
+++ customers/request.py
@@ -3,7 +3,7 @@ CUSTOMERS = [
       "id": 1,
       "name": "Hannah Hall",
       "address": "7002 Chestnut Ct",
-      "animalId": 1,
+      "customerId": 1,
       "email": "",
       "password": ""
     },
@@ -11,7 +11,7 @@ CUSTOMERS = [
       "id": 2,
       "name": "Anna Anderson",
       "address": "7054 Cherry Lane",
-      "animalId": 2,
+      "customerId": 2,
       "email": "",
       "password": ""
     },
@@ -19,7 +19,7 @@ CUSTOMERS = [
       "id": 3,
       "name": "Jenny Juice",
       "address": "7002 Chestnut Ct",
-      "animalId": 3,
+      "customerId": 3,
       "email": "",
       "password": ""
     },
@@ -37,10 +37,10 @@ def get_all_customers():
 
 # Function with a single parameter
 def get_single_customer(id):
-    # Variable to hold the found animal, if it exists
+    # Variable to hold the found customer, if it exists
     requested_customer = None
 
-    # Iterate the ANIMALS list above. Very similar to the
+    # Iterate the customerS list above. Very similar to the
     # for..of loops you used in JavaScript.
     for customer in CUSTOMERS:
         # Dictionaries in Python use [] notation to find a key
@@ -49,3 +49,19 @@ def get_single_customer(id):
             requested_customer = customer
 
     return requested_customer
+
+def create_customer(customer):
+    # Get the id value of the last customer in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the customer dictionary
+    customer["id"] = new_id
+
+    # Add the customer dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
--- employees/request.py
+++ employees/request.py
@@ -1,32 +1,30 @@
+import employees
+
+
 EMPLOYEES = [
     {
       "id": 1,
       "name": "Big Hal",
       "locationId": 2,
-      "animalId": 1
     },
     {
       "id": 2,
       "name": "Big Abe",
       "locationId": 2,
-      "animalId": 2
     },
     {
       "id": 3,
       "name": "Big Jude",
       "locationId": 1,
-      "animalId": 1
     },
     {
       "id": 4,
       "name": "Big Germ",
       "locationId": 1,
-      "animalId": 2
     },
     {
       "name": "Jake",
       "locationId": 1,
-      "animalId": 3,
       "id": 5
     }
 ]
@@ -36,10 +34,10 @@ def get_all_employees():
     return EMPLOYEES
 
 def get_single_employee(id):
-    # Variable to hold the found animal, if it exists
+    # Variable to hold the found employee, if it exists
     requested_employee = None
 
-    # Iterate the ANIMALS list above. Very similar to the
+    # Iterate the EMPLOYEES list above. Very similar to the
     # for..of loops you used in JavaScript.
     for employee in EMPLOYEES:
         # Dictionaries in Python use [] notation to find a key
@@ -48,3 +46,19 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def create_employee(employee):
+    # Get the id value of the last employee in the list
+    max_id = EMPLOYEES[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the employee dictionary
+    employee["id"] = new_id
+
+    # Add the employee dictionary to the list
+    EMPLOYEES.append(employee)
+
+    # Return the dictionary with `id` property added
+    return employee
