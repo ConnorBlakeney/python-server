8d752e6265a267c0a8a28e27392b5251c23d7716
Connor Blakeney <connorblakeney@yahoo.com>
2020-10-09 11:00:26 -0500

    setup classes but haven't done anything with them

---
+++ models/customer.py
@@ -0,0 +1,11 @@
+class Customer():
+
+    # Class initializer. It has 5 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, name, address, customerId, email, password):
+        self.name = name
+        self.address = address
+        self.customerId = customerId
+        self.email = email
+        self.password = password
